1ec710079e6b6bfc4d2b2d16971f1f609745bd27
momo <mjh79017901@gmail.com>
2020-06-28 00:59:04 +0900

    node schedule 정상 작동
    npm으로 설치할 때 node-schedule을 설치 한게 아닌 node-scheduler로 설치하여서 오류 떴었다.

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,16 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3002;
 const route = require('./routes/index');
 
-var schedule = require('node-scheduler');
+var schedule = require('node-schedule');
 
-
-var scheduler = schedule.scheduleJob("5 * * * * *", function() {
-    console.log('schedule test');
+var scheduler = schedule.scheduleJob("5,10,15,20,25,30 * * * * *", function() {
+    console.log('schedule test1');
+});
+var scheduler2 = schedule.scheduleJob("5,10,15,20,25,30 * * * * *", function() {
+    console.log('schedule test2');
+});
+var scheduler3 = schedule.scheduleJob("5,10,15,20,25,30 * * * * *", function() {
+    console.log('schedule test3');
 });
 
 //app.use(cors());
